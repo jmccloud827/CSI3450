6f0aa0b492282b6658d6752f3c900b46cc4fa8ff
jmccloud827 <60444521+jmccloud827@users.noreply.github.com>
2021-03-05 21:19:52 -0500

    Update Region.java

--- VeMan/src/VeMan/Region.java
+++ VeMan/src/VeMan/Region.java
@@ -5,6 +5,11 @@
  */
 package VeMan;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+
 /**
  *
  * @author Rich
@@ -12,24 +17,24 @@ package VeMan;
 
 
 public class Region {
+    private static ResultSet sqlResult;
 
 // Define the region name arrray and limit
-private static String[]  regionNames = new String[] {
-    "null", "Corporate", "North", "South", "Midwest", "Pacific" };
-private static int       regionCount = 5;   // Number of regions
-private static int       curRegionNum = 1;  // Used for get First/Next
+static ArrayList<String> regionNames = new ArrayList<String>();
+private static int       curRegionNum = 0;  // Used for get First/Next
 
 /* 
 * Returns the Rigion Name for a given region number
 */
 public static String getRegionName(int regionNum) {
+    getFirstRegionName();
     // Check for out of range regions
-    if ((regionNum < 1) || (regionNum > regionCount)) {
+    if ((regionNum < 0) || (regionNum > regionNames.size())) {
         return ("Reg Err: " + regionNum + "!!!");
     }
     
     // Return the region name
-    return regionNames[regionNum];
+    return regionNames.get(regionNum);
 }
     
 /* 
@@ -38,8 +43,8 @@ public static String getRegionName(int regionNum) {
 public static int getRegionNumber(String regionName) {
     //Loop through and check for match of the name
     String rname = regionName.trim();
-    for (int i=1; i<=regionCount; i++) {
-        if (rname.equalsIgnoreCase(regionNames[i]) == true) {
+    for (int i=1; i<=regionNames.size(); i++) {
+        if (rname.equalsIgnoreCase(regionNames.get(i)) == true) {
             return i;
         }
     }
@@ -54,22 +59,38 @@ public static int getRegionNumber(String regionName) {
 */
 public static String getFirstRegionName() {
     // Set the static counter to be used by getNext
-    curRegionNum = 1;
     
-    // Return the region name
-    return regionNames[curRegionNum];
+    System.out.println("In Region.GetFirstRegion.");
+        Connection dbConn = DBase.connectToDB();
+        
+        try {
+            // Build Java SQL query statement 
+            String sql = "SELECT * FROM REGION"; 
+            PreparedStatement ps = dbConn.prepareStatement(sql);
+        
+            // Send statement to mySQl to execute.
+            sqlResult = ps.executeQuery();
+            System.out.println("executeQuery complete." + sqlResult);
+        } catch(Exception e){ System.out.println("DB Error: " + e.getMessage());}
+        return getNextRegionName();
 }
 
 /* 
 * Used to enumerate the regions, Returns the next Region Name or "" if at end
 */
 public static String getNextRegionName() {
-    // Set the static counter to be used by getNext
-    curRegionNum++;
-    if(curRegionNum > regionCount)
-        return "";
-    
-    // Return the region name
-    return regionNames[curRegionNum];
+    System.out.println("In GetNextRegionName");
+        try {
+            // Read the next record from the database
+            if (sqlResult.next() != true) {
+                System.out.println("sqlResult empty.  Returning 1");
+                return "";
+            }
+        
+            // Load the data into the Region object
+            regionNames.add(sqlResult.getString("REG_NAME"));
+        } catch(Exception e){ System.out.println("DB Error" + e.getMessage());}
+            
+        return regionNames.get(curRegionNum++);
 }
 }
